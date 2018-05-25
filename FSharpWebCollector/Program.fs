30d6c0f7009ee5f21465c83cc6ad86d77cc0a3bd
Stadzior <Stadzior@gmail.com>
2018-05-25 22:11:06 +0200

    Working with SqlDataProvider for sqlite.

--- FSharpWebCollector/Program.fs
+++ FSharpWebCollector/Program.fs
@@ -2,7 +2,18 @@
 
 open System
 
+[<Literal>]
+let connectionString = "Data Source=" + __SOURCE_DIRECTORY__ + "MyIndexedWebDb.db" + "Version=3;foreign keys=true"   
+
+type sql = SqlDataProvider<
+                Common.DatabaseProviderTypes.SQLITE, 
+                SQLiteLibrary = SystemDataSQLite,
+                ConnectionString = connectionString, 
+                ResolutionPath = resolutionPath, 
+                CaseSensitivityChange = Common.CaseSensitivityChange.ORIGINAL>
+
+
+let context = sql.GetDataContext()
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
